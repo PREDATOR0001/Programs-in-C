89daaca04982b11d85f117b601721c8ab61f0374
PREDATOR0001 <98915867+PREDATOR0001@users.noreply.github.com>
2024-09-27 16:09:06 +0530

    Add files via upload

---
+++ even no. b/natural no..cpp
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+ 
+int main() { 
+    // Loop from 1 to 20 
+    for (int i = 1; i <= 20; i++) { 
+        // Check if the number is even 
+        if (i % 2 == 0) { 
+            // Print the even number 
+            printf("%d\n", i); 
+        } 
+    } 
+    return 0; 
+} 
