fc574e72024404f22c9674e0b1cc1ca234f7a829
PREDATOR0001 <98915867+PREDATOR0001@users.noreply.github.com>
2024-10-16 14:01:42 +0530

    Update checking vowels.c

--- checking vowels.c
+++ checking vowels.c
@@ -1,9 +1,7 @@
 int main()
 {
     char ch = 'A';
- 
-    // Checking if the character ch
-    // is a vowel or not.
+
     if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E'
         || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O'
         || ch == 'u' || ch == 'U') {
