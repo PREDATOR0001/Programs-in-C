b7b27a49227c628ffc6e33da04f92b41073aa729
Aditya Kumar Gupta <98915867+PREDATOR0001@users.noreply.github.com>
2024-12-06 12:06:50 +0530

    Update largest num between two num.cpp

--- largest num between two num.cpp
+++ largest num between two num.cpp
@@ -1,24 +1,22 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{ 
- int a,b,c;
- char=ch;
- printf("Enter the valve of a and b:");
- scanf("%d%d",&A,&B);
+#include <stdio.h>
 
- printf("\n addition'+'\n subtration'-' \n division'\' \n multiplication'*' \n modulus"'%');
- printf("Enter the operater=");
- scanf("%d,&ch");
- 
- int result(operator=='+')?(a+b);
-           (operator=='-')?(a-b);
-		   (operator=='*')?(a*b);
-		   (operator=='/')?(a/b);
-		   (operator=='%')?(a%b);
-		    
-		     if(operator='/'&&b==0)
-		     {
-		     	printf("Division by zero");
-		     	
-			 }
+int main() {
+    int num1, num2;
+
+    // Input two numbers from the user
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Determine and display the larger number
+    if (num1 > num2) {
+        printf("The largest number is: %d\n", num1);
+    } else if (num2 > num1) {
+        printf("The largest number is: %d\n", num2);
+    } else {
+        printf("Both numbers are equal.\n");
+    }
+
+    return 0;
+}
